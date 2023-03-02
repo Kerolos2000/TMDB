9af57ddc835ade1579df7d5a1ebc2c862d2df2ce
kerolos <kero.magdy2000@gmail.com>
2023-03-02 21:44:26 +0200

    first commit

---
+++ js/movie.js
@@ -0,0 +1,105 @@
+const myKey = "a51a2678a6ebc32710f900cff1862764";
+const imgUrl = `https://image.tmdb.org/t/p/w500`;
+const movieId = location.search.slice(4);
+const mainDiv = document.querySelector("#main-div");
+//
+fetch(
+  `https://api.themoviedb.org/3/movie/${movieId}?api_key=${myKey}&language=en-US`
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    let arr = [];
+    let companies = "company";
+    for (var i = 0; i < response.production_companies.length; i++) {
+      arr.push(response.production_companies[i].name);
+      if (response.production_companies.length > 1) {
+        companies = "companies";
+      }
+    }
+    mainDiv.innerHTML = `
+        <div class="col-lg-6 left">
+          <div class="img">
+            <img src="${imgUrl}${
+      response.poster_path
+    }" class="img-fluid" alt="${response.title}">
+          </div>
+        </div>
+        <div class="col-lg-6 right">
+          <div class="img">
+          ${
+            response.backdrop_path
+              ? `<img src="${
+                  imgUrl + response.backdrop_path
+                }" class="img-fluid hide-img" alt="${response.title}">`
+              : ``
+          }
+          </div>
+          <h2>${response.title || response.original_title}</h2>
+          <p>${response.overview}</p>
+          <div class="d-flex flex-wrap">
+            <p class="me-3">vote average : ${response.vote_average.toFixed(
+              1
+            )}</p>
+            <p>vote count : ${response.vote_count}</p>
+          </div>
+          <p>production ${companies} : ${arr.join(" | ")}</p>
+          <p>release date : ${response.release_date}</p>
+
+
+
+            ${
+              response.homepage
+                ? `
+                <button class="learn-more">
+                  <span class="circle" aria-hidden="true">
+                    <span class="icon arrow"></span>
+                  </span>
+                  <a class="button-text" href="${response.homepage}">home page</a>
+                </button>
+                `
+                : ``
+            }
+
+
+        </div>
+  `;
+  });
+
+// on search get all movie contain search value
+const SearchInput = document.querySelector("#Search");
+const SearchBtn = document.querySelector("#Search-btn");
+SearchBtn.addEventListener("click", () => {
+  location.href = `./index.html?id=${SearchInput.value}`;
+});
+
+let similar = $("#similar .swiper-wrapper");
+fetch(
+  `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${myKey}&language=en-US&page=1`
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    response = response.results;
+    for (let i = 0; i < response.length; i++) {
+      similar[0].innerHTML += `
+      <div class="swiper-slide">
+        <div class="all-card">
+          <a class="ancor" href="../movie.html?id=${response[i].id}">
+            <div class="cardX">
+              <div class="img">
+                <img src="${imgUrl}${
+        response[i].poster_path
+      }" class="img-fluid" alt="">
+              </div>
+              <div class="text">
+                <h5>${response[i].title || response[i].original_title}</h5>
+                <p>8.5</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+      `;
+    }
+  });
---
+++ js/search.js
@@ -0,0 +1,85 @@
+const myKey = "a51a2678a6ebc32710f900cff1862764";
+const imgUrl = `https://image.tmdb.org/t/p/w500/`;
+const movieId = location.search.slice(4);
+const mainDiv = document.querySelector("#main-div");
+const SearchInput = document.querySelector("#Search");
+const SearchBtn = document.querySelector("#Search-btn");
+const loader = document.querySelector("#loader");
+// console.log(movieId);
+
+function getApi(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((response) => {
+      // console.log(response);
+      loader.classList.remove("show-loading");
+      let temp = "";
+      for (let i = 0; i < response.results.length; i++) {
+        temp += `
+        <div class="all-card col-sm-6 col-lg-4 col-xl-3">
+          <a class="ancor" href="../movie.html?id=${response.results[i].id}">
+            <div class="cardX">
+            <div class="img">
+              <img src="${imgUrl}${
+          response.results[i].poster_path
+        }" class="img-fluid" alt="${response.results[i].title}">
+            </div>
+              <div class="text">
+                <h5>${
+                  response.results[i].title ||
+                  response.results[i].original_title ||
+                  response.results[i].name
+                }</h5>
+                <p>${response.results[i].vote_average.toFixed(1)}</p>
+              </div>
+              <div class="float-div">
+                <h5>Overview</h5>
+                <p>${response.results[i].overview}</p>
+              </div>
+            </div>
+          </a>
+        </div>
+        `;
+      }
+      mainDiv.innerHTML = temp;
+    });
+}
+if (movieId != "") {
+  getApi(
+    `https://api.themoviedb.org/3/search/movie?api_key=${myKey}&language=en-US&query=${movieId}&page=1&include_adult=false`
+  );
+  SearchInput.value = movieId;
+} else {
+  getApi(`https://api.themoviedb.org/3/trending/all/day?api_key=${myKey}`);
+}
+
+// on search get all movie contain search value
+SearchBtn.addEventListener("click", () => {
+  mainDiv.innerHTML = "";
+  loader.classList.add("show-loading");
+  getApi(
+    `https://api.themoviedb.org/3/search/movie?api_key=${myKey}&language=en-US&query=${Search.value}&page=1&include_adult=false`
+  );
+  if (Search.value == "") {
+    getApi(`https://api.themoviedb.org/3/trending/all/day?api_key=${myKey}`);
+  }
+});
+
+//
+$(".badge").click(function () {
+  loader.classList.add("show-loading");
+  mainDiv.innerHTML = "";
+  let newTarget = this.dataset.target; // this.dataset.target = e.target.dataset.target
+  let badge = Array.from($(".badge"));
+  badge.forEach((el) => {
+    el.classList.remove("active");
+  });
+  this.classList.add("active");
+  if (newTarget == "trending") {
+    getApi(`https://api.themoviedb.org/3/trending/all/day?api_key=${myKey}`);
+  } else {
+    getApi(
+      `https://api.themoviedb.org/3/movie/${newTarget}?api_key=${myKey}&language=en-US&page=1`
+    );
+  }
+});
